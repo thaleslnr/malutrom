496836862249320e23fe58e7aa25d54b447b14e9
thaleslnr <44848784+thaleslnr@users.noreply.github.com>
2018-11-24 20:30:01 -0200

    Update install.sh

--- codedeploy/install.sh
+++ codedeploy/install.sh
@@ -1,2 +1,2 @@
-cp /var/www/html/malutrom/codeploy/zz_malutrom.conf /etc/apache2/sites-available/
-a2ensite zz_malutrom
+cp /var/www/html/malutrom/codeploy/zz_malutrom.conf /etc/httpd/conf.d/
+service httpd restart
